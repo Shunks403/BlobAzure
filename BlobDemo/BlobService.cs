ee4f9c95d0de58da08b571cc48797fd1e687aef2
anilluffi <anilluffi@gmail.com>
2024-09-13 09:58:18 +0300

    first commit

---
+++ BlobDemo/BlobService.cs
@@ -0,0 +1,172 @@
+﻿using System.Text.Json;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.Sas;
+
+namespace BlobDemo;
+
+internal class BlobService
+{
+    private readonly string _connectionString;
+
+    public BlobService(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public async Task<BlobContainerClient> GetContainer(string name)
+    {
+        BlobServiceClient blobServiceClient = new BlobServiceClient(_connectionString);
+        BlobContainerClient container = blobServiceClient.GetBlobContainerClient(name);
+        await container.CreateIfNotExistsAsync();
+        return container;
+    }
+
+    public async Task AddBlob(BlobContainerClient container, string path)
+    {
+        var name = Path.GetFileName(path);
+        BlobClient blobClient = container.GetBlobClient(name);
+
+        if(!File.Exists(path))
+        {
+            throw new FileNotFoundException("file not found");
+        }
+
+        await blobClient.UploadAsync(path);
+        Console.WriteLine($"Blob '{name}' was uploaded to Azure");
+    }
+
+    public async Task DisplayBlobs(BlobContainerClient container)
+    {
+        Console.WriteLine("Name\t\tLast Modified\tAccess tier\tSize");
+        await foreach(var blob in container.GetBlobsAsync())
+        {
+            double size = blob.Properties.ContentLength!.Value / 1024.0;
+            Console.WriteLine($"{blob.Name}\t\t{blob.Properties.LastModified!.Value.DateTime.ToShortTimeString()}\t\t{blob.Properties.AccessTier}\t\t{size.ToString("F2")} KiB");
+        }
+    }
+
+    public async Task SetBlobAccessTier(BlobContainerClient container, string name, AccessTier accessTier)
+    {
+        BlobClient blobClient = container.GetBlobClient(name);
+        await blobClient.SetAccessTierAsync(accessTier);
+    }
+
+    public async Task SetBlobMetadata(BlobContainerClient container, string name, IDictionary<string, string> metadata)
+    {
+        BlobClient blobClient = container.GetBlobClient(name);
+        await blobClient.SetMetadataAsync(metadata, null, default);
+    }
+
+    public async Task DeleteBlob(BlobContainerClient container, string name)
+    {
+        BlobClient blobClient = container.GetBlobClient(name);
+        await blobClient.DeleteIfExistsAsync();
+    }
+
+    public async Task AddSnapshot(BlobContainerClient container, string name)
+    {
+        BlobClient blobClient = container.GetBlobClient(name);
+        await blobClient.CreateSnapshotAsync();
+    }
+
+    public async Task<string> GetSAS(BlobContainerClient container, string name)
+    {
+        BlobClient blobClient = container.GetBlobClient(name);
+
+        if (!blobClient.CanGenerateSasUri)
+        {
+            throw new ArgumentException("blob cannot generate sas");
+        }
+        BlobSasBuilder builder = new BlobSasBuilder()
+        {
+            BlobContainerName = container.Name,
+            BlobName = name,
+            Resource = "b",
+            ExpiresOn = DateTime.UtcNow.AddMinutes(10),
+        };
+        builder.SetPermissions(BlobAccountSasPermissions.Read | BlobAccountSasPermissions.Write);
+        Uri uri = blobClient.GenerateSasUri(builder);
+
+        return uri.ToString();
+    }
+
+    public async Task DownloadBlob(BlobContainerClient container, string name)
+    {
+        BlobClient blobClient = container.GetBlobClient(name);
+
+        if (!Directory.Exists("data"))
+        {
+            Directory.CreateDirectory("data");
+        }
+
+        await blobClient.DownloadToAsync(Path.Combine("data", name));
+    }
+
+    public async Task DeleteMultipleBlobs(BlobContainerClient container, IEnumerable<string> names)
+    {
+        BlobBatchClient blobBatchClient = container.GetBlobBatchClient();
+        int count = names.Count();
+        List<Uri> uris = new List<Uri>(capacity: count);
+        foreach (string name in names)
+        {
+            var blob = container.GetBlobClient(name);
+            uris.Add(blob.Uri);
+        }
+
+        await blobBatchClient.DeleteBlobsAsync(uris);
+    }
+    
+    //дз
+    public async Task UploadObjectAsync<T>(BlobContainerClient container, string blobName, T obj)
+    {
+        var blobClient = container.GetBlobClient(blobName);
+
+        
+        string jsonString = JsonSerializer.Serialize(obj);
+
+        
+        using (var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(jsonString)))
+        {
+            await blobClient.UploadAsync(stream, true);
+        }
+
+        
+        var metadata = new Dictionary<string, string>
+        {
+            { "ObjectType", typeof(T).FullName } 
+        };
+        await blobClient.SetMetadataAsync(metadata);
+    }
+
+   
+    public async Task<T> DownloadObjectAsync<T>(BlobContainerClient container, string blobName)
+    {
+        var blobClient = container.GetBlobClient(blobName);
+
+       
+        var downloadInfo = await blobClient.DownloadAsync();
+        using (var streamReader = new StreamReader(downloadInfo.Value.Content))
+        {
+            string jsonString = await streamReader.ReadToEndAsync();
+
+            
+            return JsonSerializer.Deserialize<T>(jsonString);
+        }
+    }
+
+    
+    public async Task<string> GetBlobObjectTypeAsync(BlobContainerClient container, string blobName)
+    {
+        var blobClient = container.GetBlobClient(blobName);
+        var properties = await blobClient.GetPropertiesAsync();
+
+       
+        if (properties.Value.Metadata.TryGetValue("ObjectType", out string objectType))
+        {
+            return objectType;
+        }
+        return null;
+    }
+}
---
+++ BlobDemo/Program.cs
@@ -0,0 +1,73 @@
+﻿using BlobDemo;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+            .AddJsonFile("config.json")
+            .Build();
+
+string connectionString = config.GetConnectionString("Default") ?? throw new NullReferenceException("Connection string not found");
+
+var blobService = new BlobService(connectionString);
+
+try
+{
+    var container = await blobService.GetContainer("temp2");
+    string blobName = "myBlob";
+
+    Cat cat = new Cat { Name = "simona", Sound = "мау" };
+    
+    await blobService.UploadObjectAsync(container, blobName, cat);
+    Console.WriteLine("Объект отправлен в Blob Storage");
+
+    
+    string objectType = await blobService.GetBlobObjectTypeAsync(container, blobName);
+    Console.WriteLine($"Тип объекта, сохранённого в Blob: {objectType}");
+
+    
+    var downloadedObject = await blobService.DownloadObjectAsync<Cat>(container, blobName);
+    Console.WriteLine($"Объект получен: {downloadedObject.Name}, {downloadedObject.Sound}");
+    
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
+/*try
+{
+    var container = await blobService.GetContainer("temp2");
+    string path = "";
+
+
+    for(int i = 0; i < 5; i++)
+    {
+        File.Copy(Path.Combine(path, "np.jpg"), Path.Combine(path, $"np{i}.jpg"));
+        await blobService.AddBlob(container, Path.Combine(path, $"np{i}.jpg"));
+    }
+
+    await blobService.DisplayBlobs(container);
+
+    await blobService.DeleteMultipleBlobs(container, 
+        Enumerable.Range(0, 5).Select(i => $"np{i}.jpg"));
+
+    Console.WriteLine(new string('-', 40));
+    await blobService.DisplayBlobs(container);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Произошла ошибка: {ex.Message}");
+}*/
+
+
+
+
+class Cat
+{
+   public string Name { get; set; }
+    
+   public string Sound { get; set; }
+    
+}
